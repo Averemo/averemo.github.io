ca0f5de18f7c4623d1edee97a5540188724e3e20
Phil Burk <philburk@mobileer.com>
2025-07-19 15:25:37 -0700

    Experiment with local menus in JavaScript

--- docs/index.md
+++
@@ -1,6 +0,0 @@
-# Averemo
-
-Promoting shared truth.
-
-What does "Averemo" mean? It is a made-up word that is intended to mean
-"truth to me". It is also Italian for "we will have".
--- docs/object.js
+++ docs/object.js
@@ -1,5 +1,17 @@
-function tryMe(arg) {
-    document.write(arg);
-    document.write("<br/>");
-    document.write(arg);
+function getBase() {
+    return window.location.hostname === 'localhost'
+        ? '/~phil/averemo'
+        : '';
+}
+
+function buildLink(url, text) {
+    return '<a href="' + getBase() + url + '">' + text + '</a>';
+}
+function testMenus() {
+
+    document.write(buildLink('/index.html', 'Home'))
+    document.write('&nbsp;|&nbsp')
+    document.write(buildLink('/test.html', 'Constitution'))
+    document.write('&nbsp;|&nbsp')
+    document.write(buildLink('/topics/elfraud.html', 'Election Fraud'))
 }
